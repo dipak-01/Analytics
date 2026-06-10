fd2fe1de29e75b7b3b9ad936d61194acf86ffb94
dipak-01 <imt_2022037@iiitm.ac.in>
2026-06-10 12:49:43 +0530

    checks: drizzle connection setup and working

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
 // Database connection - Neon + Drizzle
 import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 import dotenv from "dotenv"
 dotenv.config();
 
@@ -8,5 +9,5 @@ if (!process.env.DATABASE_URL) {
 }
 
 const sql = neon(process.env.DATABASE_URL);
-
-export { sql };
+const db = drizzle({ client: sql });
+export { sql,db };
